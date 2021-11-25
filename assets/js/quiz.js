4ecc00b083234a85ea35883a7e79a76a9365dd27
Joshua Lintz <jplintz@gmail.com>
2021-11-25 10:34:36 -0700

    fixed grammatical error in one of the questions.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -20,7 +20,7 @@ var questions = [
         answer: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
